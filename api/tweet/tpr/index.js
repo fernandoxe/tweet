282df97e1235df1ec4edefec980e21fa0a1d2551
Fernando Parra <fernandoxe@gmail.com>
2021-08-02 01:26:27 -0300

    feat: post random tweet

--- api/tweet/tpr/index.js
+++ api/tweet/tpr/index.js
@@ -1,5 +1,25 @@
 const tpr = require('../../../data/tpr');
+const config = require('../../../.config');
+const Twit = require('twit');
+
+const T = new Twit({
+  consumer_key: config.tpr.consumer_key,
+  consumer_secret: config.tpr.consumer_secret,
+  access_token: config.tpr.access_token,
+  access_token_secret: config.tpr.access_token_secret,
+});
+
+function getRandomTweet(tweets) {
+  return tweets[Math.floor(Math.random() * tweets.length)];
+}
 
 module.exports = (req, res) => {
-  res.status(200).send(tpr[0]);
+  const tweet = getRandomTweet(tpr);
+  T.post('statuses/update', { status: tweet })
+  .then((result) => {
+    res.status(200).send(`Post ok: ${result.data.text}`);
+  })
+  .catch(err => {
+    res.status(500).send(`Error: ${err.message} (${err.code})`);
+  });
 };
